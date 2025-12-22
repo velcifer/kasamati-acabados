0a14aa802e9bd586f33e2127085db22919dceb4b
Julio A <50281349+velcifer@users.noreply.github.com>
2025-12-22 00:05:09 -0500

    Ultimos cambios-13

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 // 🚀 VERCEL SERVERLESS FUNCTION - KSAMATI API
 // Este archivo es el punto de entrada para las funciones serverless de Vercel
 
-const app = require('../server/index');
+//const app = require('../server/index');
 
 // Exportar como función serverless de Vercel
-module.exports = app;
+//module.exports = app;
 
